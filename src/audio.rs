9ae0f4a87b8a97f4084eb4c72fd8caf7c40e5b70
Vojislav Stojkovic <zinthys@gmail.com>
2022-02-14 09:27:41 -0800

    Add support for memory-backed audio streams

--- src/audio.rs
+++ src/audio.rs
@@ -1,12 +1,15 @@
 use std::path::Path;
+use std::ptr::null;
 
 use windows as Windows;
-use Windows::core::GUID;
+use Windows::core::{GUID, HRESULT};
+use Windows::Win32::Foundation::E_OUTOFMEMORY;
 use Windows::Win32::Media::Audio::{WAVEFORMATEX, WAVE_FORMAT_PCM};
 use Windows::Win32::Media::Speech::{
     ISpStream, SpStream, SPFILEMODE, SPFM_CREATE_ALWAYS, SPFM_OPEN_READONLY,
 };
-use Windows::Win32::System::Com::{CoCreateInstance, CLSCTX_ALL};
+use Windows::Win32::System::Com::{CoCreateInstance, IStream, CLSCTX_ALL};
+use Windows::Win32::UI::Shell::SHCreateMemStream;
 
 use crate::com_util::Intf;
 use crate::Result;
@@ -74,6 +77,14 @@ impl AudioStream {
         Self::from_file(path, format, SPFM_CREATE_ALWAYS)
     }
 
+    pub fn from_stream<S: Into<IStream>>(stream: S, format: &AudioFormat) -> Result<Self> {
+        let intf: ISpStream = unsafe { CoCreateInstance(&SpStream, None, CLSCTX_ALL) }?;
+        unsafe { intf.SetBaseStream(stream.into(), &SPDFID_WaveFormatEx, &format.to_sapi()) }?;
+        Ok(Self {
+            intf: Intf(intf),
+        })
+    }
+
     fn from_file<P: AsRef<Path>>(path: P, format: &AudioFormat, mode: SPFILEMODE) -> Result<Self> {
         let intf: ISpStream = unsafe { CoCreateInstance(&SpStream, None, CLSCTX_ALL) }?;
         unsafe {
@@ -92,5 +103,36 @@ impl AudioStream {
 
     pub(crate) fn to_sapi(&self) -> ISpStream {
         self.intf.0.clone()
+    }
+}
+
+pub struct MemoryStream {
+    intf: Intf<IStream>,
+}
+
+impl MemoryStream {
+    pub fn new(init_data: Option<&[u8]>) -> Result<Self> {
+        Ok(Self {
+            intf: Intf(Self::create_stream(init_data)?),
+        })
+    }
+
+    pub fn try_clone(&self) -> Result<Self> {
+        unsafe { self.intf.Clone() }.map(|intf| Self {
+            intf: Intf(intf),
+        })
+    }
+
+    fn create_stream(init_data: Option<&[u8]>) -> std::result::Result<IStream, HRESULT> {
+        let size =
+            init_data.map(|buf| buf.len()).unwrap_or(0).try_into().map_err(|_| E_OUTOFMEMORY)?;
+        unsafe { SHCreateMemStream(init_data.map(|buf| buf.as_ptr()).unwrap_or(null()), size) }
+            .ok_or(E_OUTOFMEMORY)
+    }
+}
+
+impl From<MemoryStream> for IStream {
+    fn from(source: MemoryStream) -> Self {
+        source.intf.0
     }
 }
