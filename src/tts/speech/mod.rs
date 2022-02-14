b61643c3af2a6a3776511cf7a76e83cb241605cc
Vojislav Stojkovic <zinthys@gmail.com>
2022-02-14 15:54:10 -0800

    Split up tts::speech

---
+++ src/tts/speech/mod.rs
@@ -0,0 +1,43 @@
+use std::borrow::{Borrow, Cow};
+
+use windows as Windows;
+use Windows::Win32::Media::Speech::{SPF_DEFAULT, SPF_IS_XML, SPF_PARSE_SAPI};
+
+mod builder;
+mod types;
+
+pub use builder::SpeechBuilder;
+pub use types::{Pitch, Rate, SayAs, Volume};
+
+pub enum Speech<'s> {
+    Text(Cow<'s, str>),
+    Xml(Cow<'s, str>),
+}
+
+impl<'s> Speech<'s> {
+    pub(crate) fn flags(&self) -> u32 {
+        (match self {
+            Self::Text(_) => SPF_DEFAULT.0,
+            Self::Xml(_) => (SPF_IS_XML.0 | SPF_PARSE_SAPI.0),
+        }) as u32
+    }
+
+    pub(crate) fn contents(&self) -> &str {
+        match self {
+            Self::Text(cow) => cow.borrow(),
+            Self::Xml(cow) => cow.borrow(),
+        }
+    }
+}
+
+impl<'s> From<&'s str> for Speech<'s> {
+    fn from(s: &'s str) -> Self {
+        Self::Text(s.into())
+    }
+}
+
+impl<'s> From<String> for Speech<'s> {
+    fn from(s: String) -> Self {
+        Self::Text(s.into())
+    }
+}
---
+++ src/tts/speech/types.rs
@@ -0,0 +1,91 @@
+use std::fmt::Display;
+use std::hash::Hash;
+
+pub enum SayAs<'s> {
+    DateMDY,
+    DateDMY,
+    DateYMD,
+    DateYM,
+    DateMY,
+    DateDM,
+    DateMD,
+    DateYear,
+    Time,
+    NumberCardinal,
+    NumberDigit,
+    NumberFraction,
+    NumberDecimal,
+    PhoneNumber,
+    Custom(&'s str),
+}
+
+impl<'s> SayAs<'s> {
+    pub(super) fn sapi_id(&self) -> &str {
+        match self {
+            Self::DateMDY => "date_mdy",
+            Self::DateDMY => "date_dmy",
+            Self::DateYMD => "date_ymd",
+            Self::DateYM => "date_ym",
+            Self::DateMY => "date_my",
+            Self::DateDM => "date_dm",
+            Self::DateMD => "date_md",
+            Self::DateYear => "date_year",
+            Self::Time => "time",
+            Self::NumberCardinal => "number_cardinal",
+            Self::NumberDigit => "number_digit",
+            Self::NumberFraction => "number_fraction",
+            Self::NumberDecimal => "number_decimal",
+            Self::PhoneNumber => "phone_number",
+            Self::Custom(s) => s,
+        }
+    }
+}
+
+macro_rules! decl_clamped_int {
+    {$name:ident($base:ty) in $min:literal..$max:literal} => {
+        #[derive(Hash, PartialEq, Eq, PartialOrd, Ord)]
+        pub struct $name($base);
+
+        impl $name {
+            pub fn new(value: $base) -> Self {
+                Self(value.clamp($min, $max))
+            }
+
+            pub fn value(&self) -> $base {
+                self.0
+            }
+        }
+
+        impl From<$base> for $name {
+            fn from(source: $base) -> Self {
+                Self::new(source)
+            }
+        }
+
+        impl From<$name> for $base {
+            fn from(source: $name) -> Self {
+                source.0
+            }
+        }
+
+        impl Display for $name {
+            fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+                write!(f, "{}", self.0)
+            }
+        }
+    };
+}
+
+decl_clamped_int! { Pitch(i32) in -10..10 }
+decl_clamped_int! { Rate(i32) in -10..10 }
+decl_clamped_int! { Volume(u32) in 0..100 }
+
+impl Volume {
+    pub(crate) fn from_sapi(source: u16) -> Self {
+        Self::new(source as _)
+    }
+
+    pub(crate) fn sapi_value(&self) -> u16 {
+        self.0 as _
+    }
+}
